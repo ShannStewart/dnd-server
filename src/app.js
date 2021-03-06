f8bdfadb663a5725d9b64e2e56b975689a54fe0c
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2021-03-05 22:34:25 -0500

    set up db

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+//const { CLIENT_ORIGIN } = require('./config');
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+
+const authRouter = require('./auth/auth-router')
+//const quizzesRouter = require('./quizzes/quizzes-router')
 
 const app = express()
 
@@ -14,6 +18,9 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use('/api/auth', authRouter)
+//app.use('/api/quizzes/', quizzesRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
